44ec2e64081d1f548e3d8845da694de7615a8e54
Richard Y. Hayashi <richardyhayashi@gmail.com>
2024-10-09 23:38:59 +0900

    Refactor

--- src/react-from-first-principles/react/src/index.js
+++ src/react-from-first-principles/react/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+createRoot(rootElement).render(<App />);
